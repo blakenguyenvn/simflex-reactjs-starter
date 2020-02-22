28d7a0f633a001b1c56823d479a1dbc99fffd653
Tuan Nguyen <tuan.minh91vn@gmail.com>
2020-02-23 00:19:44 +0700

    Update repo config and add config for firebase

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAPV4K2ZIEwbR901w06pGwk6nW2UVsNdjw",
+  authDomain: "simflexbase.firebaseapp.com",
+  databaseURL: "https://simflexbase.firebaseio.com",
+  projectId: "simflexbase",
+  storageBucket: "simflexbase.appspot.com",
+  messagingSenderId: "910131771110",
+  appId: "1:910131771110:web:5ac93155a4838d88caad27",
+  measurementId: "G-1JGSH6L1ZD"
+};
+
+export default firebaseConfig;
